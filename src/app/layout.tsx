014eb2d3f69c4aa08bde8271c341d28a7af3f449
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-26 11:36:54 -0400

    remove padding from body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import buttons from '@/data/buttons.json';
 import headerTabs from '@/data/headertabs.json';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import colors from '@/colors';
+import { Box, Stack } from '@mui/material';
 
 export const metadata = {
     title: 'ngallant.dev'
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
-            <body>
+            <body style={{ margin: '0', padding: '0' }}>
                 <Header tabs={headerTabs} />
                 {children}
                 <Footer iconButtons={buttons} />
