25809978d53be63f39049f7babeb6469f2339cc0
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-26 13:17:00 -0400

    Change accent color

--- src/colors.ts
+++ src/colors.ts
@@ -1,9 +1,10 @@
 const colors = {
-    accent: '#F5A9B8',
+    pink: '#F5A9B8',
     background: '#D3D3D3',
     white: '#ffffff',
     black: '#000000',
-    yellow: '#F5E85F'
+    yellow: '#F5E85F',
+    accent: '#FAE033'
 };
 
 export default colors;
