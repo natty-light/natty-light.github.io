3937bec0e435f5e7e6b5ffe39c38afa980f9aef7
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-26 11:06:04 -0400

    add new color

--- src/colors.ts
+++ src/colors.ts
@@ -2,7 +2,8 @@ const colors = {
     accent: '#F5A9B8',
     background: '#D3D3D3',
     white: '#ffffff',
-    black: '#000000'
+    black: '#000000',
+    yellow: '#F5E85F'
 };
 
 export default colors;
