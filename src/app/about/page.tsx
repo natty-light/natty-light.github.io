2db871ad0b588170208298b00169b72e305759c2
Natasha <75773163+natty-light@users.noreply.github.com>
2024-03-29 00:30:08 -0400

    Make pages consistent with container

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,46 +5,54 @@ import Island from '@/components/Island';
 import useScreenSize from '@/hooks/useScreenSize';
 import { Typography } from '@mui/material';
 import { FC } from 'react';
+import { Container } from '../styles';
 
 const About: FC = () => {
     const { isMobile } = useScreenSize();
     return (
-        <Island>
-            <Typography variant={isMobile ? 'h3' : 'h1'} color={colors.accent}>
-                about me
-            </Typography>
-            <Typography
-                padding="8px"
-                variant="body1"
-                textAlign="center"
-                alignSelf="center"
-                color={colors.white}
-            >
-                i&apos;m a full stack developer. i have a degree in physics
-                which i obtained from James Madison Univerity in Harrisonburg,
-                Virginia. i currently live in Annandale, Virginia. i&apos;m
-                employed as a full stack engineer at Exclusive Resorts. my
-                langauges of choice at Go and TypeScript. i&apos;m comfortable
-                with a variety of technologies like React, GraphQL, Docker, and
-                Salesforce. some cool projects i&apos;ve worked on include
-                contributing to full stack web apps, designing, implementing,
-                and project-managing a custom data pipeline functionality.
-                i&apos;m particularly interested in the field of programming
-                language design and implementation, as well as graphics
-                programming with tools like OpenGL.
-            </Typography>
-            <Typography
-                padding="8px"
-                variant="body1"
-                textAlign="center"
-                alignSelf="center"
-                color={colors.white}
-            >
-                outside of work, i enjoy cycling and fencing, a good cup of
-                coffee, and french and italian cooking. i&apos;m a big fan of
-                stand-up comedy, (watching, not doing), and i love movies.
-            </Typography>
-        </Island>
+        <Container>
+            <Island>
+                <Typography
+                    variant={isMobile ? 'h3' : 'h1'}
+                    color={colors.accent}
+                >
+                    about me
+                </Typography>
+                <Typography
+                    padding="8px"
+                    variant="body1"
+                    textAlign="center"
+                    alignSelf="center"
+                    color={colors.white}
+                >
+                    i&apos;m a full stack developer. i have a degree in physics
+                    which i obtained from James Madison Univerity in
+                    Harrisonburg, Virginia. i currently live in Annandale,
+                    Virginia. i&apos;m employed as a full stack engineer at
+                    Exclusive Resorts. my langauges of choice at Go and
+                    TypeScript. i&apos;m comfortable with a variety of
+                    technologies like React, GraphQL, Docker, and Salesforce.
+                    some cool projects i&apos;ve worked on include contributing
+                    to full stack web apps, designing, implementing, and
+                    project-managing a custom data pipeline functionality.
+                    i&apos;m particularly interested in the field of programming
+                    language design and implementation, as well as graphics
+                    programming with tools like OpenGL.
+                </Typography>
+                <Typography
+                    padding="8px"
+                    variant="body1"
+                    textAlign="center"
+                    alignSelf="center"
+                    color={colors.white}
+                >
+                    outside of work, i enjoy cycling and fencing, a good cup of
+                    coffee, and french and italian cooking. i&apos;m a big fan
+                    of stand-up comedy, (watching, not doing), and i love
+                    movies.
+                </Typography>
+            </Island>
+        </Container>
     );
 };
 
--- src/components/Island/styles.ts
+++ src/components/Island/styles.ts
@@ -2,7 +2,6 @@ import colors from '@/colors';
 import { Stack, styled } from '@mui/material';
 
 export const Container = styled(Stack)(({ theme }) => ({
-    height: 'auto',
     width: '80%',
     justifyItems: 'center',
     alignItems: 'center',
@@ -10,5 +9,5 @@ export const Container = styled(Stack)(({ theme }) => ({
     borderRadius: '10px',
     padding: '32px 16px',
     boxShadow: `0 0 10px ${colors.accent}`,
-    margin: 'auto auto'
+    flex: '1'
 }));
